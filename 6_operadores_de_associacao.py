f3c20ef172b7360a042d7790697622a5ea66ec08
Dalila Custódio <dalila.dalila70@gmail.com>
2024-04-22 18:13:48 -0300

    code: 6_operadores_de_associacao

---
+++ 6_operadores_de_associacao.py
@@ -0,0 +1,10 @@
+# in (para saber se contem na lista)
+# not in (para saber se não contem na lista)
+
+frutas = ["limao", "uva"]
+curso = "Curso de Python"
+
+print("laranja" not in frutas) # Result: True. 
+print("limao" in frutas) # Result: True.
+print("Python" in curso) # Result: True.
+print("python" in curso) # Result: False.
