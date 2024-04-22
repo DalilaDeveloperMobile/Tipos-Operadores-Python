c136b1e92d8fef503fe03a0aab343954e531ed13
Dalila Custódio <dalila.dalila70@gmail.com>
2024-04-22 18:10:08 -0300

    code: 1_operadores_aritméticos

---
+++ 1_operadores_aritméticos.py
@@ -0,0 +1,31 @@
+produto_1 = 20
+produto_2 = 10
+
+# Adição
+print(produto_1 + produto_2) # Result: 30
+
+# Subtração
+print(produto_1 - produto_2) # Result: 10
+
+# Divisão
+print(produto_1 / produto_2) # Result: 2.0
+
+# Divisão Inteira
+print(produto_1 // produto_2) # Result: 2
+
+# Multiplicação
+print(produto_1 * produto_2) # Result: 200
+
+# Módulo
+print(produto_1 % produto_2) # Result: 0
+
+# Exponenciação
+print(produto_1 ** produto_2) # Result: 10240000000000
+
+# Precedência de Operadores
+# Pode Usar () Para Definir
+# Precedencia ou Deixar Mais Claro.  
+x = 10 + (5 * 4) # Result: 30
+y = (10 / 2) + (25 * 2) - (2 ** 2) # Result: 51.0
+print(x)
+print(y)
