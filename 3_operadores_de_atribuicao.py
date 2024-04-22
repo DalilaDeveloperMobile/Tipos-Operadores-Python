aeb4d88df1316319fca3b74d5fa36ea08871451a
Dalila Custódio <dalila.dalila70@gmail.com>
2024-04-22 18:11:41 -0300

    code: 3_operadores_de_atribuicao

---
+++ 3_operadores_de_atribuicao.py
@@ -0,0 +1,33 @@
+# Atribuição Simples
+saldo = 500
+print(saldo) # Result: 500 
+saldo = 200
+print(saldo) # Result: 200 
+
+# Atribuição com Adição
+saldo += 10
+print(saldo) # Result: 210 
+
+# Atribuição com Subtração
+saldo -= 5
+print(saldo) # Result: 205
+
+# Atribuição com Divisão Inteira
+saldo //= 2
+print(saldo) # Result: 102
+
+# Atribuição com Divisão
+saldo /= 2
+print(saldo) # Result: 51.0
+
+# Atribuição com Multiplicação
+saldo *= 10
+print(saldo) # Result: 510.0
+
+# Atribuição com Modularização
+saldo %= 4
+print(saldo) # Result: 2.0
+
+# Atribuição com Exponenciação
+saldo **= 2
+print(saldo) # Result: 4.0
