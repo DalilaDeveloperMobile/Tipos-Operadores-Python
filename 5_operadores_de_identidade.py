ae999291efa09c63cc15a565b1dea04c475dcdf4
Dalila Custódio <dalila.dalila70@gmail.com>
2024-04-22 18:13:03 -0300

    code: 5_operadores_de_identidade

---
+++ 5_operadores_de_identidade.py
@@ -0,0 +1,8 @@
+saldo = 1000
+limite = 1000
+
+# is (se ocupa a mesma região de memória).
+# is not (se não ocupa a mesma região de memória).
+
+print(saldo is limite) # Result: True. 
+print(saldo is not limite) # Result: False.
