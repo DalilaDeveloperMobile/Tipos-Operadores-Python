8bf753aa7fc5b64af5d54024e3da2cda942f301f
Dalila Custódio <dalila.dalila70@gmail.com>
2024-04-22 18:10:58 -0300

    code: 2_operadores_de_comparação

---
+++ 2_operadores_de_comparação.py
@@ -0,0 +1,20 @@
+saldo = 200
+saque = 200
+
+# Igualdade
+print(saldo == saque) # Result: True
+
+# Diferença
+print(saldo != saque) # Result: False
+
+# Maior Que
+print(saldo > saque) # Result: False
+
+# Maior ou Igual
+print(saldo >= saque) # Result: True
+
+# Menor Que
+print(saldo < saque) # Result: False
+
+# Menor ou Igual
+print(saldo <= saque) # Result: True
