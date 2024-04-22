462fb23776406080185aed57ba5a650c54529ec3
Dalila Custódio <dalila.dalila70@gmail.com>
2024-04-22 18:12:18 -0300

    code: 4_operadores_logicos

---
+++ 4_operadores_logicos.py
@@ -0,0 +1,12 @@
+# AND = Para ser True tudo tem que ser True.
+# OR = Para ser True apenas um tem que ser True.
+
+# E = AND
+print(True and True) # Result: True.
+print(True and False) # Result: False.
+print(False and False) # Result: False.
+
+# OU = OR
+print(True or True) # Result: True.
+print(True or False) # Result: True.
+print(False or False) # Result: False.
